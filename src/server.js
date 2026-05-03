f1ea7c2157fec2348d647d34a7c6392d07a0929b
shlok612 <shlokkatiyar62@gmail.com>
2026-05-04 00:19:50 +0530

    added vercel frontend

--- src/server.js
+++ src/server.js
@@ -11,7 +11,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*"
+    origin: [
+      "http://localhost:5173",
+      "https://quickdrop-frontend-6mlu.vercel.app"
+    ],
+    methods: ["GET", "POST"]
   }
 });
 
